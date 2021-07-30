b697e925bc1a3390944d8828552201855821cb6b
jeanmmlima <jeanmariomlima@gmail.com>
2021-07-30 19:17:01 -0300

    middleware para remover valores em cascata modulo de impressora 3d

--- models/UsuarioImp3D.js
+++ models/UsuarioImp3D.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+require("./ReservaImp3D")
+const ReservaImp3D = mongoose.model("reservasimp3d")
 
 const UsuarioImp3D = new Schema({
 
@@ -18,5 +20,15 @@ const UsuarioImp3D = new Schema({
         default: 1
     }
 })
+
+//middleware para registro que dependam do usuário imp 3d
+UsuarioImp3D.pre('deleteOne', function(next) {
+    // 'this' is the client being removed. Provide callbacks here if you want
+    // to be notified of the calls' result.
+    const id = this.getQuery()['_id'];
+    //console.log(id);
+    ReservaImp3D.deleteMany({usuario3d: id}).exec();
+    next();
+});
 
 mongoose.model("usuariosimp3d", UsuarioImp3D)
